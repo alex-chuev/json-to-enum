10dc220de846417c20efb79fc4305450af64a066
Alex Chuev <alex@chuev.com>
2020-01-11 13:43:05 +0300

    Remove unused libraries and file watching

--- src/commands/compile.ts
+++ src/commands/compile.ts
@@ -3,7 +3,7 @@ import { Arguments, Options } from 'yargs'
 import { EnumValue } from '../enums/EnumValue'
 import { FilenameCase } from '../enums/FilenameCase'
 import { Compiler, CompilerConfig } from '../entities/Compiler'
-import glob from 'glob'
+import { watch } from 'chokidar'
 import { EnumConfig } from '../interfaces/EnumConfig'
 import { existsSync } from 'fs'
 import { resolve } from 'path'
@@ -20,41 +20,59 @@ export const builder: { [key: string]: Options } = {
   input: {
     alias: 'i',
     description: 'Glob pattern for input files',
+    string: true,
   },
   outputFolder: {
     alias: 'o',
+    string: true,
   },
   config: {
     alias: 'c',
     default: 'json-to-enum.js',
+    string: true,
+  },
+  watch: {
+    alias: 'w',
+    default: false,
+    boolean: true,
   },
   enumFilenameCase: {
     default: FilenameCase.Kebab,
+    string: true,
   },
   enumFilenameEnding: {
     default: '.ts',
+    string: true,
   },
   enumValue: {
     default: EnumValue.Path,
+    string: true,
   },
   enumTabs: {
     default: false,
+    boolean: true,
   },
   enumSpaces: {
     default: 2,
+    number: true,
   },
   enumExportDefault: {
     default: true,
+    boolean: true,
   },
   enumPathValueSeparator: {
     default: '.',
+    string: true,
   },
   enumValueQuotes: {
     default: "'",
+    string: true,
   },
 }
 
 export function handler(args: Arguments<CompileArgs>): void {
+  const compile = (file: string) => new Compiler(file).compile(args)
+
   if (existsSync(args.config)) {
     try {
       const configFromFile: Partial<EnumConfig & CompilerConfig> = require(resolve(args.config))
@@ -67,8 +85,10 @@ export function handler(args: Arguments<CompileArgs>): void {
   }
 
   if (args.input) {
-    glob.sync(args.input).forEach(file => {
-      new Compiler(file).compile(args)
+    watch(args.input, {
+      persistent: args.watch,
     })
+      .on('add', file => compile(file))
+      .on('change', file => compile(file))
   }
 }
